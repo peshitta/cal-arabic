221fe48cadd1fe069625996ebf1ea948caac7344
Greg Borota <borota@gmail.com>
2017-10-17 02:40:02 -0500

    Initial checkin

---
+++ src/main.js
@@ -0,0 +1,127 @@
+/** @module calArabic */
+import { Writing, Mapper } from 'aramaic-mapper';
+import {
+  allConsonants as calConsonants,
+  vowels as calVowels,
+  diacritics as calDiacritics,
+  isDotted as isCalDotted
+} from 'cal-code-util';
+import {
+  consonants as arabicConsonants,
+  vowels as arabicVowels
+} from 'arabic-code-util';
+
+/**
+ * @private
+ * CAL source writing
+ * @const
+ * @type { Writing }
+ */
+const calWriting = new Writing(calConsonants, calVowels, calDiacritics);
+
+const feh = '\u0641'; // ف ARABIC LETTER FEH
+const seen = '\u0633'; // س ARABIC LETTER SEEN
+
+/**
+ * @private
+ * Arabic destination writing
+ * @const
+ * @type { Writing }
+ */
+const arabicWriting = new Writing(
+  Object.freeze(arabicConsonants.concat(feh, seen)),
+  Object.freeze([
+    arabicVowels[0], // a: fatha
+    '', // o: callback will use superscript alef or alef maddah as needed
+    arabicVowels[2], // e: triple kasra to cover `e` and `i` and `E`
+    arabicVowels[2], // i
+    arabicVowels[3], // u: duplicate damma to cover both `u` and `O`
+    arabicVowels[2], // E
+    arabicVowels[3] // O:
+  ]),
+  Object.freeze(['', '', '', ''])
+);
+
+/**
+ * @private
+ * Maps input character to Arabic char
+ * @param { string } c input character
+ * @param { Object.<string, string> } fromTo mapping dictionary
+ * @returns { string } Arabic mapped char
+ */
+const to = (c, fromTo) => fromTo[c] || (fromTo[c] === '' ? '' : c);
+const alef = '\u0627'; // ا ARABIC LETTER ALEF
+const fatha = '\u064E'; //  َ Arabic fatha - Garshuni: a
+const supAlef = '\u0670'; //  ٰ Arabic letter superscript alef - Garshuni: long a
+const kasra = '\u0650'; //  ِ Arabic kasra - Garshuni: i
+const damma = '\u064F'; //  ُ Arabic damma - Garshuni: u
+const waw = '\u0648'; // و ARABIC LETTER WAW
+const yeh = '\u064A'; // ي ARABIC LETTER YEH
+const madda = '\u0622'; // آ ARABIC LETTER ALEF WITH MADDA ABOVE
+
+/**
+ * @private
+ * Maps input character to CAL char
+ * @param { string } c Syriac input character
+ * @param { Object.<string, string> } fromTo mapping dictionary
+ * @param { Object } wordProps optional word settings
+ * @returns { string } CAL mapped char
+ */
+const mapCallback = (word, i, fromTo, wordProps) => {
+  const c = word.charAt(i);
+  if (!wordProps.isDotted) {
+    return to(c, fromTo);
+  }
+  let m = '';
+  const n = word.charAt(i + 1);
+  switch (c) {
+    case 'y':
+      m =
+        n === 'i' || n === 'e'
+          ? kasra + yeh // Arabic stores as (iy)
+          : to(c, fromTo);
+      break;
+    case 'w':
+      m =
+        n === 'u' || n === 'O'
+          ? damma + waw // Arabic stores as (uw)
+          : to(c, fromTo);
+      break;
+    case ')':
+      m = n === 'o' ? madda : alef;
+      break;
+    case 'o':
+      // using alef subscript to avoid introducing new alef not found in source
+      m =
+        word.charAt(i - 1) === ')'
+          ? '' // handled already as madda
+          : n === ')' ? fatha : supAlef;
+      break;
+    default:
+      m = to(c, fromTo);
+      break;
+  }
+  return m;
+};
+
+/**
+ * Arabic Mapper
+ * @const
+ * @type { Mapper }
+ */
+export const mapper = new Mapper(calWriting, arabicWriting, mapCallback);
+
+/**
+ * Convert from CAL to Arabic
+ * @static
+ * @param {string} word input word in CAL code transliteration
+ * @returns {string} the input word converted to Arabic Unicode
+ */
+export const toArabic = word => {
+  const wordProp = Object.freeze(
+    Object.create(null, {
+      isDotted: { value: isCalDotted(word), enumerable: true }
+    })
+  );
+  return mapper.map(word, wordProp);
+};
---
+++ test/index_test.js
@@ -0,0 +1,241 @@
+const test = require('assert');
+const sut = require('../build/cal-arabic');
+
+describe('CAL', () => {
+  describe('To Arabic', () => {
+    it('General case usage, with one-to-one mapping', () => {
+      const word = sut.toArabic('dqsry)-dpylypws');
+      const wordExpected = 'دقسريا-دفيليفوس';
+      const vocalised = sut.toArabic("d'qesariya)-d,p,yilyip'wOs");
+      const vocalisedExpected = 'دقِسَرِيَا-دفِيلِيفُوس';
+      test.strictEqual(word, wordExpected, 'sut.toArabic_generic consonant');
+      test.strictEqual(
+        vocalised,
+        vocalisedExpected,
+        'sut.toArabic_generic vocalised'
+      );
+    });
+    it('Madda case', () => {
+      const word = sut.toArabic('))r');
+      const wordExpected = 'اار';
+      const vocalised = sut.toArabic(')o)ar');
+      const vocalisedExpected = 'آاَر';
+      test.strictEqual(word, wordExpected, 'sut.toArabic_madda consonant');
+      test.strictEqual(
+        vocalised,
+        vocalisedExpected,
+        'sut.toArabic_madda vocalised'
+      );
+    });
+    it('Alef superscript', () => {
+      const word = sut.toArabic('$mk');
+      const wordExpected = 'شمك';
+      const vocalised = sut.toArabic('$mok');
+      const vocalisedExpected = 'شمٰك';
+      test.strictEqual(word, wordExpected, 'sut.toArabic_madda consonant');
+      test.strictEqual(
+        vocalised,
+        vocalisedExpected,
+        'sut.toArabic_madda vocalised'
+      );
+    });
+    it('Word with (yi) => (i;) mapping', () => {
+      const word = sut.toArabic('dylydwth');
+      const wordExpected = 'ديليدوته';
+      const vocalised = sut.toArabic("d'yilyid,wut,ehE");
+      const vocalisedExpected = 'دِيلِيدُوتِهِ';
+      test.strictEqual(word, wordExpected, 'sut.toArabic_yi consonant');
+      test.strictEqual(
+        vocalised,
+        vocalisedExpected,
+        'sut.toArabic_yi vocalised'
+      );
+    });
+    it('Word with short Eastern (E) => (e) mapping', () => {
+      const word = sut.toArabic(')wld');
+      const wordExpected = 'اولد';
+      const vocalised = sut.toArabic(')awlEd');
+      const vocalisedExpected = 'اَولِد';
+      test.strictEqual(word, wordExpected, 'sut.toArabic_yi consonant');
+      test.strictEqual(
+        vocalised,
+        vocalisedExpected,
+        'sut.toArabic_yi vocalised'
+      );
+    });
+    it('Word with (wu) => (uO) mapping', () => {
+      const word = sut.toArabic('lb(ldbbykwn');
+      const wordExpected = 'لبعلدببيكون';
+      const vocalised = sut.toArabic("lab,(eld'b,ob,ayk'wun");
+      const vocalisedExpected = 'لَبعِلدبٰبَيكُون';
+      test.strictEqual(word, wordExpected, 'sut.toArabic_wu consonant');
+      test.strictEqual(
+        vocalised,
+        vocalisedExpected,
+        'sut.toArabic_wu vocalised'
+      );
+    });
+    it('Word with (wO) => (oO) mapping', () => {
+      const word = sut.toArabic(')bhwhy');
+      const vocalised = sut.toArabic(')ab,ohawh_y');
+      const wordExpected = 'ابهوهي';
+      const vocalisedExpected = 'اَبٰهَوهي';
+      test.strictEqual(word, wordExpected, 'sut.toArabic_wO consonant');
+      test.strictEqual(
+        vocalised,
+        vocalisedExpected,
+        'sut.toArabic_wO vocalised'
+      );
+    });
+    it('Word with Palestinian P', () => {
+      const word = sut.toArabic(')Pbhwhy');
+      const vocalised = sut.toArabic(')aPeb,ohawh_y');
+      const wordExpected = 'افبهوهي';
+      const vocalisedExpected = 'اَفِبٰهَوهي';
+      test.strictEqual(word, wordExpected, 'sut.toArabic_wO consonant');
+      test.strictEqual(
+        vocalised,
+        vocalisedExpected,
+        'sut.toArabic vocalised with P'
+      );
+    });
+    it('Word with Hebrew Sin', () => {
+      const word = sut.toArabic(')Pbhwh&');
+      const wordExpected = 'افبهوهس';
+      const vocalised = sut.toArabic(')aPeb,ohawh_&o');
+      const vocalisedExpected = 'اَفِبٰهَوهسٰ';
+      test.strictEqual(word, wordExpected, 'sut.toArabic consonant with Sin');
+      test.strictEqual(
+        vocalised,
+        vocalisedExpected,
+        'sut.toArabic vocalised with Sin'
+      );
+    });
+    it('Word with Hebrew Shin', () => {
+      const word = sut.toArabic(')d$)');
+      const wordExpected = 'ادشا';
+      const vocalised = sut.toArabic(')od,$o)');
+      const vocalisedExpected = 'آدشَا';
+      test.strictEqual(word, wordExpected, 'sut.toArabic consonant with Shin');
+      test.strictEqual(
+        vocalised,
+        vocalisedExpected,
+        'sut.toArabic vocalised with Shin'
+      );
+    });
+    it('Invalid Rukkakha', () => {
+      const word = sut.toArabic(')Pbh,swh&');
+      const wordExpected = 'افبهسوهس';
+      test.strictEqual(word, wordExpected, 'sut.toArabic invalid rukkakha');
+    });
+    it('Standalone Ou', () => {
+      const word = sut.toArabic(')PbOh,suwh&');
+      const wordExpected = 'افبُهسُوهس';
+      test.strictEqual(word, wordExpected, 'sut.toArabic invalid rukkakha');
+    });
+    it('Blank word returns blank', () => {
+      const word = sut.toArabic('');
+      const wordExpected = '';
+      test.strictEqual(word, wordExpected, 'sut.toArabic_blank');
+    });
+    it('Null word returns null', () => {
+      const word = sut.toArabic(null);
+      const wordExpected = null;
+      test.strictEqual(word, wordExpected, 'sut.toArabic_null');
+    });
+    it('Undefined word returns undefined', () => {
+      const word = sut.toArabic(undefined);
+      const wordExpected = undefined;
+      test.strictEqual(word, wordExpected, 'sut.toArabic_undefined');
+    });
+    it('0 number as word returns 0', () => {
+      const word = sut.toArabic(0);
+      const wordExpected = 0;
+      test.strictEqual(word, wordExpected, 'sut.toArabic_zero');
+    });
+  });
+  describe('Mapped writing', () => {
+    it('Consonants length', () => {
+      test.strictEqual(
+        sut.mapper.fromWriting.consonants.length,
+        sut.mapper.toWriting.consonants.length,
+        'Length differs'
+      );
+      test.ok(
+        sut.mapper.fromWriting.consonants.length > 22,
+        'Length greater than 22'
+      );
+    });
+    it('Vowels length', () => {
+      test.strictEqual(
+        sut.mapper.fromWriting.vowels.length,
+        sut.mapper.toWriting.vowels.length,
+        'Length differs'
+      );
+      test.ok(
+        sut.mapper.fromWriting.vowels.length > 5,
+        'Length greater than 5'
+      );
+    });
+  });
+});
+
+describe('CAL', () => {
+  describe('To Arabic', () => {
+    it('Blank word returns blank', () => {
+      const word = sut.toArabic('');
+      const wordExpected = '';
+      test.strictEqual(word, wordExpected, 'sut.toArabic_blank');
+    });
+    it('Null word returns null', () => {
+      const word = sut.toArabic(null);
+      const wordExpected = null;
+      test.strictEqual(word, wordExpected, 'sut.toArabic_null');
+    });
+    it('Undefined word returns undefined', () => {
+      const word = sut.toArabic(undefined);
+      const wordExpected = undefined;
+      test.strictEqual(word, wordExpected, 'sut.toArabic_undefined');
+    });
+    it('0 number as word returns 0', () => {
+      const word = sut.toArabic(0);
+      const wordExpected = 0;
+      test.strictEqual(word, wordExpected, 'sut.toArabic_zero');
+    });
+  });
+  describe('Mapped writing', () => {
+    it('Consonants length', () => {
+      test.strictEqual(
+        sut.mapper.fromWriting.consonants.length,
+        sut.mapper.toWriting.consonants.length,
+        'Length differs'
+      );
+      test.ok(
+        sut.mapper.fromWriting.consonants.length > 22,
+        'Length greater than 22'
+      );
+    });
+    it('Vowels length', () => {
+      test.strictEqual(
+        sut.mapper.fromWriting.vowels.length,
+        sut.mapper.toWriting.vowels.length,
+        'Length differs'
+      );
+      test.ok(
+        sut.mapper.fromWriting.vowels.length > 5,
+        'Length greater than 5'
+      );
+    });
+    it('Diacritics length', () => {
+      test.strictEqual(
+        sut.mapper.fromWriting.diacritics.length,
+        sut.mapper.toWriting.diacritics.length,
+        'Length differs'
+      );
+      test.ok(
+        sut.mapper.fromWriting.diacritics.length = 4,
+        'Length equals 4'
+      );
+    });
+  });
+});
